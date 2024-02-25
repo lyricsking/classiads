87124aaa8390b4096323a429df774f9de8c9bd65
Jamiu Adeniyi <30894401+lyricsking@users.noreply.github.com>
2024-02-25 13:18:47 +0100

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,7 +4,31 @@ export default {
   content: ['./app/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {},
-  },
+  },daisyui: {
+  themes: [
+        {
+          mytheme: {
+          
+ "primary": "#fbbf24",
+          
+ "secondary": "#f3f4f6",
+          
+ "accent": "#fde68a",
+          
+ "neutral": "#e5e7eb",
+          
+ "base-100": "#d1d5db",
+          
+ "info": "#fef3c7",
+          
+ "success": "#00ff00",
+          
+ "warning": "#cffafe",
+          
+ "error": "#ff0000",
+          },
+        },
+      ],},
   plugins: [
     require('daisyui')
   ],
